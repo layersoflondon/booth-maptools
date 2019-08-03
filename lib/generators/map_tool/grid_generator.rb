e25e5682a1ea9876728b1536788db4c7557726f3
Ed Jones <ed@error.agency>
2019-08-03 18:44:33 +0100

    move grid square maths into configuration

--- lib/generators/map_tool/grid_generator.rb
+++ lib/generators/map_tool/grid_generator.rb
@@ -3,16 +3,12 @@ module MapTool
     desc "Generate grid squares in a pattern from the configured north_west_extent to the south_east_extent, of size square_size"
     def create_squares
       config = LayersOfLondon::Booth::MapTool.configuration
-      north_west = Geokit::LatLng.new(*config.north_west_extent)
-      south_east = Geokit::LatLng.new(*config.south_east_extent)
-      north_east = Geokit::LatLng.new(north_west.lat, south_east.lng)
-      south_west = Geokit::LatLng.new(south_east.lat, north_west.lng)
 
-      squares_x = (north_west.distance_to(north_east, units: :meters) / config.square_size).ceil
-      squares_y = (north_west.distance_to(south_west, units: :meters) / config.square_size).ceil
-
+      squares_y = config.squares_y
+      squares_x = config.squares_x
+      north_west = config.north_west
       row_north_west = north_west
-      square_north_west = north_west
+
       squares_y.times do |row|
         puts "Creating row #{row}"
           square_north_west = row_north_west
@@ -20,10 +16,10 @@ module MapTool
           puts "\tCreating column #{col}"
           LayersOfLondon::Booth::MapTool::Square.create(north_west_lat: square_north_west.lat, north_west_lng: square_north_west.lng)
           # get the next square's northwest corner by moving east (90º) by the number of columns from the row's northwest
-          square_north_west = square_north_west.endpoint(90, config.square_size + 1, units: :meters)
+          square_north_west = square_north_west.endpoint(90, config.square_size, units: :meters)
         end
         # increment row_north_west by square_size, southwards, ready for the next iteration
-        row_north_west = row_north_west.endpoint(180, config.square_size + 1, units: :meters)
+        row_north_west = row_north_west.endpoint(180, config.square_size, units: :meters)
       end
 
     end
